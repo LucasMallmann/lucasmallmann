6a03b319be82ac7731246fff46a6651a943c1772
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-11-05 20:06:06 -0300

    refactor: open sans font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Head from 'next/head';
 import { GetStaticProps, NextPage } from 'next';
+import {
+  Heading,
+  Grid,
+  Flex,
+  Link,
+  Button,
+  Text,
+  Input,
+  Divider,
+  Box,
+} from '@chakra-ui/core';
 import { PostsOrPages } from '@tryghost/content-api';
 
 import api from 'services/api';
-import { Heading } from '@chakra-ui/core';
 
 async function getPosts(): Promise<PostsOrPages> {
   const posts = await api.posts.browse({ order: 'created_at DESC' });
@@ -15,25 +25,29 @@ interface Props {
   posts: PostsOrPages;
 }
 
-const Home: NextPage<Props> = ({ posts }) => (
-  <div>
-    <Head>
-      <title>Home page</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home: NextPage<Props> = ({ posts }) => {
+  console.log(posts);
 
-    <Heading size="md">Hello blog</Heading>
-
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>
-          <strong>{post.title}</strong>
-          <span>{post.created_at}</span>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+  return (
+    <div>
+      <Head>
+        <title>Home page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Heading size="md">Hello blog</Heading>
+      <Box
+        marginY={4}
+        marginX="auto"
+        backgroundColor="purple.500"
+        width="50%"
+        paddingX={2}
+        paddingY={2}
+      >
+        Tomato
+      </Box>
+    </div>
+  );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const posts = await getPosts();
