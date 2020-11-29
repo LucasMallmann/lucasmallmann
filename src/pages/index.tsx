2c97b465c923c8b4f618971493ca489cf5f413dc
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-11-29 12:10:26 -0300

    refactor: responsive styles to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,52 +23,52 @@ const Home: NextPage<Props> = ({ posts }) => {
   console.log(posts);
 
   return (
-    <>
+    <Flex direction="column" padding={[4, 4, 0, 0]}>
       <Head>
         <title>Home page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Flex direction="column" alignItems="center">
         <Heading
           as="h1"
-          fontSize="5xl"
+          fontSize={['3xl', '3xl', '4xl', '5xl']}
           letterSpacing="wide"
           fontWeight={600}
-          marginTop={9}
+          marginTop={[2, 2, 9, 9]}
+          textAlign="center"
         >
           Lucas Mallmann
         </Heading>
         <Text
           color="pink.500"
           fontStyle="italic"
-          fontSize="xl"
+          fontSize={['sm', 'md', 'xl']}
           fontWeight="semi-bold"
-          marginTop={4}
-          // borderBottomWidth="2px"
-          // borderBottomColor="purple.500"
+          marginTop={[2, 4]}
+          textAlign="center"
         >
           Ensinando as pessoas a se apaixonarem cada vez mais por tecnologia!
         </Text>
       </Flex>
 
-      <Box marginTop={9}>
-        <Heading as="h1" fontSize="4xl" fontWeight="normal">
+      <Box marginTop={[4, 4, 9, 9]}>
+        <Heading as="h1" fontSize={['3xl', '3xl', '4xl']} fontWeight="normal">
           Quem sou eu?
         </Heading>
 
-        <Text as="p" marginY={PARAGRAPH_MARGIN}>
+        <Text as="p" marginY={PARAGRAPH_MARGIN} lineHeight={6}>
           Olá, seja muito bem vindo(a) ao meu blog! Meu nome é Lucas Mallmann.
           Trabalho como engenheiro de software desde 2017. Acredito que a
           educação, a divulgação de conhecimento e de boas ideias é o caminho
           mais rápido e eficaz para melhorar o mundo!
         </Text>
 
-        <Text as="p" marginY={PARAGRAPH_MARGIN}>
+        <Text as="p" marginY={PARAGRAPH_MARGIN} lineHeight={6}>
           Atualmente trabalho como Software Enginner na{' '}
           <Link
             href="https://pling.net.br"
             isExternal
-            // color="purple.500"
             color="pink.500"
             fontWeight="600"
             _hover={{
@@ -86,14 +86,14 @@ const Home: NextPage<Props> = ({ posts }) => {
           aprender seja ensinar.
         </Text>
 
-        <Text as="p" marginY={PARAGRAPH_MARGIN}>
+        <Text as="p" marginY={PARAGRAPH_MARGIN} lineHeight={6}>
           Gosto muito de música, apaixonado por games, um aventureiro na
           culinária, e nos tempos livres adoro filosofar sobre a vida. Apesar de
           ser um blog voltado para tecnologia, não se surpreenda se trouxermos
           algumas reflexões por aqui.
         </Text>
 
-        <Text as="p" marginY={PARAGRAPH_MARGIN}>
+        <Text as="p" marginY={PARAGRAPH_MARGIN} lineHeight={6}>
           Muitos sonhos e objetivos meus nasceram de recursos online que eu
           encontrei sobre tecnologia. Acredito que seja o meu dever, e eu
           ficarei extremamente feliz se conseguir retribuir pra vocês um pouco
@@ -108,7 +108,7 @@ const Home: NextPage<Props> = ({ posts }) => {
       <Box marginTop={6}>
         <ArticleCard />
       </Box>
-    </>
+    </Flex>
   );
 };
 
