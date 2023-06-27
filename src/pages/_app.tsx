f519828e84afcfba4db944635bf30c32733d7a7d
Lucas Mallmann <lucasmallmann76@gmail.com>
2023-06-26 22:39:37 -0300

    add ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ import MDXComponents from 'components/MDXComponents';
  * Configs
  */
 import SEO from 'config/next-seo.config';
+import Script from 'next/script';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -27,15 +28,33 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <ThemeProvider attribute="class">
-      <MDXProvider components={MDXComponents}>
-        <Head>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-        </Head>
-        <DefaultSeo {...SEO} />
-        {getLayout(<Component {...pageProps} />)}
-      </MDXProvider>
-    </ThemeProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`}
+      </Script>
+
+      <ThemeProvider attribute="class">
+        <MDXProvider components={MDXComponents}>
+          <Head>
+            <meta
+              content="width=device-width, initial-scale=1"
+              name="viewport"
+            />
+          </Head>
+          <DefaultSeo {...SEO} />
+          {getLayout(<Component {...pageProps} />)}
+        </MDXProvider>
+      </ThemeProvider>
+    </>
   );
 };
 
